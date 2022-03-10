e8843603dcf3ca415e2c05928f148f49a12eadde
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-10 11:52:35 +0700

    cek lagi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
