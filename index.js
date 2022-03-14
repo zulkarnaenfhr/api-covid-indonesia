c9457b7b12bd448cee8f9bab9cda45f5b1d3a7ac
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-14 15:48:48 +0700

    bismillah feed berita

--- index.js
+++ index.js
@@ -10,23 +10,33 @@ const corsOptions = {
     origin: ["http://localhost:3000", "https://zulkarnaenfhr.github.io/indonesia-covid-mapping/", "https://zulkarnaenfhr.github.io/"],
 };
 
-const apiCityRiskScore = "https://data.covid19.go.id/public/api/skor.json";
-
 app.get("/", cors(corsOptions), async (req, res) => {
     const url = "localhost/5000";
     res.json({
         message: "Just to Enable the Cors Option from https://covid19.go.id/dokumentasi-api . for personal project because there is a corsOption url setting just for my project",
         endpoint: {
             cityScore: "https://api-covid-indonesia.herokuapp.com/cityScore",
+            feedBerita: "https://api-covid-indonesia.herokuapp.com/feedBerita",
         },
     });
 });
 
+const apiCityRiskScore = "https://data.covid19.go.id/public/api/skor.json";
 app.get("/cityScore", cors(corsOptions), async (req, res) => {
     const fetchOptions = {
         method: "GET",
     };
     const response = await fetch(apiCityRiskScore, fetchOptions);
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
+});
+
+const feedBerita = "https://newsapi.org/v2/everything?q=covid&apiKey=4f8b1414b1634e198f8758eef4d8daf2&domains=detik.com";
+app.get("/feedBerita", cors(corsOptions), async (req, res) => {
+    const fetchOptions = {
+        method: "GET",
+    };
+    const response = await fetch(feedBerita, fetchOptions);
     const jsonResponse = await response.json();
     res.json(jsonResponse);
 });
